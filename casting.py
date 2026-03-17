7a0c676c72b92181f24064bb43a0f58e9a3e1584
juanapisa <jipisa@mail.austral.edu.ar>
2026-03-17 11:05:37 -0300

    Update casting.py

--- casting.py
+++ casting.py
@@ -5,13 +5,19 @@ def casting():
     precio = input("ingrese el precio ")
     precio_int = int(precio)
     print("Precio:", precio)
+
     descuento = input("ingrese descuento ")
     descuento_int = int(descuento)
     print("Decuento: ",descuento)
-    cantidad = input("ingrese la cantidad de entradas que quiere ")
+
+    cantidad = input("ingrese la cantidad de entradas: ")
     cantidad_int = int(cantidad)
+
     calcular_descuento = precio_int * (descuento_int / 100)
     precio_con_descuento = precio_int - calcular_descuento
     print("Precio con descuento:", precio_con_descuento)
-    calcular_total = (precio_int * cantidad_int)- calcular_descuento
-    print("Total:", calcular_total)
+
+    precio_sin_descuento = (precio_int * cantidad_int)
+    calcular_total =  precio_sin_descuento * (descuento_int / 100)
+    total = precio_sin_descuento - calcular_total
+    print("Total:", total)
