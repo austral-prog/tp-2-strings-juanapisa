b41b2224ae3f8a46a4d3ef43b141accde4155dda
juanapisa <jipisa@mail.austral.edu.ar>
2026-03-27 18:24:35 -0300

    Update string_info.py

--- string_info.py
+++ string_info.py
@@ -5,7 +5,9 @@ def string_info():
     palabra = "Programacion"
     print("Palabra:",palabra)
     print("Longitud:", len("Programacion"))
-    print("Primera Letra:", palabra[0])
-    print("Ultima Letra:", palabra[-1])
-    print("Retetida:", palabra*3)
-    print("Decorada:", f" ***{palabra}***" )
+    print("Primera letra:", palabra[0])
+    print("Ultima letra:", palabra[-1])
+    print("Repetida:", palabra*3)
+    print("Decorada:", f"***{palabra}***" )
+
+
