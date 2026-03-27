742fdbdd6925e141543f5b46c0de4456472e7fa0
juanapisa <jipisa@mail.austral.edu.ar>
2026-03-27 18:22:48 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,11 +2,13 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
-    nombre = input("ingresa tu nombre")
-    apellido = input("ingresa tu nombre")
+    
+    nombre = input()
+    apellido = input()
     print(nombre.lower(), apellido.lower())
     print(nombre.title(), apellido.title())
     print(nombre.upper(), apellido.upper())
     mensaje = f"\t{nombre.lower()} {apellido.lower()}"
     print(mensaje)
+
+
