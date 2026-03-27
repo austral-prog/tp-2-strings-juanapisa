5db097ba120ffa5547fac5cca3c694322cbbcca2
juanapisa <jipisa@mail.austral.edu.ar>
2026-03-27 11:21:51 -0300

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -8,3 +8,19 @@ def string_methods():
     multilinea = """Linea 1
     Linea 2
     Linea 3"""
+    print("Strip:", nombre.strip())
+    print("Lstrip:", nombre.lstrip())
+    print("Rstrip:", nombre.rstrip())
+    print("Upper:", frase.upper())
+    print("Lower:", frase.lower())
+    print("Title:", frase.title())
+    print("Find:",frase.find("gran"))
+    print("Replace:",frase.replace("programacion", "desarrllo"))
+    print("Count:",frase.count("a"))
+    print("Contiene Python:", "Python" in frase)
+    print("Contiene Java:", "Java" in frase)
+    print("Slice:", frase[0:6])
+    print("Paso:", frase[0:6:2])
+    print("Reverso:", frase[5::-1])
+    print("Formato:", f"{nombre.strip()}, sabe python")
+    print(multilinea)
