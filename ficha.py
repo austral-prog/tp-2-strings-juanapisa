bc26917172c67164dfb7b970091be89a6dd198eb
juanapisa <jipisa@mail.austral.edu.ar>
2026-03-31 11:13:17 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -1,8 +1,3 @@
-from idlelib.replace import replace
-from itertools import count
-from urllib.parse import uses_query
-
-
 def ficha():
     """Ejercicio integrador. Lee nombre, email y 3 notas, y genera una ficha
     de alumno aplicando: strip, title, lower, upper, int, len, find, slicing,
